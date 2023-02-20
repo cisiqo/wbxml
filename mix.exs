2324909f15929bd1841c439128b8e8eda97716a2
cisiqo <csq1124@gmail.com>
2023-02-20 11:48:34 +0800

    Update mix info

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Wbxml.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package()
     ]
   end
 
@@ -18,11 +19,19 @@ defmodule Wbxml.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp  description() do
+    "A wbxml library."
+  end
+
+  defp package() do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # This option is only needed when you don't want to use the OTP application name
+      name: "wbxml",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README*  LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/cisiqo/wbxml"}
     ]
   end
+
 end
